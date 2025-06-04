991c2403dcd907bdb8e145dda2dfa90bd0ce11f8
Karri Lumivirta <lumivirta.karri@hotmail.com>
2025-06-04 14:05:57 +0300

    Add check for empty strings in cleaned words, add more forbidden chars to exclusion

--- src/data_handler.py
+++ src/data_handler.py
@@ -7,12 +7,19 @@ class DataHandler:
         self.data = []
 
     def read(self):
+        '''
+        Handles opening and reading the source file.
+        '''
+
         with io.open(self.source,"r", encoding="utf-8") as file:
             raw_data = file.read()
 
         return raw_data
 
     def process(self):
+        '''
+        Processes the given file into a list of words, separated by spaces. Removes non-letter characters.
+        '''
 
         raw_data = self.read()
 
@@ -20,11 +27,11 @@ class DataHandler:
 
         #Tekstin käsittely haasteellista, jotain häikkää åäö-merkkien tunnistuksessa ja printtauksessa.
         for words in raw_words:
-            clean_word = "".join([c for c in words if c not in " @_!#$%^&*()<>?/|}{~:[]"])
-        
-            self.data.append(clean_word)
+            clean_word = "".join([c for c in words if c not in "0123456789 -.,@_!#$%^&*()<>?/|}{~:[]"])
+            if clean_word == "":
+                continue
+            self.data.append(clean_word.lower())
         
 
         return self.data
-
 
