8d1bc3bfcda1da502077cb35313c92f87d426a57
Karri Lumivirta <lumivirta.karri@hotmail.com>
2025-06-05 13:13:43 +0300

    Make usage more user friendly

--- src/main.py
+++ src/main.py
@@ -4,9 +4,10 @@ import sys
 def main():
 
     source = sys.argv[1]
-    degree = int(sys.argv[2])
-    words_to_generate = int(sys.argv[3])
-    word_length = int(sys.argv[4])
+    print("Please select generation specifics.")
+    degree = int(input("What degree chain do you wish to use?: "))
+    words_to_generate = int(input("How many words to generate?: "))
+    word_length = int(input("How long should the words be?: "))
 
     markov_generator = Generator(source,degree,word_length)
 
@@ -21,8 +22,17 @@ def main():
         print(word," : ", len(word))
     
     print("Do you wish to save these words?")
-    save = input("y/n?:")
+    
 
+    save = input("y/n?: ")
+    
+    if save not in "yn":
+        while True:
+            print("Invalid input, please reply y or n.")
+            save = input("y/n?: ")
+            if save == "y" or save =="n":
+                break
+    
     if save == "y":
         generated_words_as_string = "\n".join(generated_words)
         with open("generated_words.txt", "w") as file:
@@ -32,9 +42,9 @@ def main():
     elif save == "n":
         print("No words saved.")
     
-    else:
-        print("Invalid input, please repeat.")
-        save = input("y/n?:")
+    #else:
+    #    print("Invalid input, please repeat.")
+    #    save = input("y/n?:")
 
     print("Thanks for using the generator.")
 
